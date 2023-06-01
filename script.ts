04bbfbbfabc8cceeb06dbd838a511412ed8b7edb
andreyka101 <johndoe@example.com>
2023-06-01 21:22:38 +0300

    gtjgjgj

--- script.ts
+++ script.ts
@@ -1,11 +1,30 @@
-// dz
+// pz 1 /////////////////////////////////////////////
 
 
 // 1
 
 function n1 (){
+  let rectangle ={
+    topLeftY : 1,
+    topLeftX : 1,
+    lowerRightY : 3,
+    lowerRightX : 3,
+  }
+}
+n1()
+
+
+
+// dz 1 /////////////////////////////////////////////
+
+
+
+// 1
+
+function n2 (){
   let car ={
     predictor : "Proton Edar Sdr Holding",
     model : "PROTON PERDANA",
   }
 }
+n2()
