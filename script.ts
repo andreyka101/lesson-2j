209d262c31bf1bafa98cd8f8bbfc9037f2d7ce03
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-02 09:54:11 +0300

    efeffe

--- script.ts
+++ script.ts
@@ -10,6 +10,14 @@ function n1 (){
     lowerRightY : 3,
     lowerRightX : 3,
   }
+  localStorage.obj = JSON.stringify({
+    topLeftY : 1,
+topLeftX : 1,
+lowerRightY : 3,
+lowerRightX : 3,
+  })
+  
+  console.log(JSON.parse(localStorage.obj))
 }
 n1()
 
