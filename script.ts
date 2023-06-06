4973337fb856732abbec5423bfbbccb73bdb2855
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-06 10:15:45 +0300

    lsehfsflsfb

--- script.ts
+++ script.ts
@@ -1,4 +1,3 @@
-console.log("ggggg")
 
 // dz 1 /////////////////////////////////////////////
 
@@ -11,6 +10,37 @@ let car ={
   model : "PROTON PERDANA",
   yearOfIssue : 2008 ,
   speed : 260 ,
+}
+
+const answer1 = document.querySelector('.block1 #s1')
+
+function n1 (){
+  let strcar = "ответ : "
+  for (let i in car){
+    strcar += car[i]
+    strcar += " // "
+  }
+  // @ts-ignore
+  answer1.innerText = strcar
+}
+
+function n2 (){
+  let distance = parseInt(prompt('введите расстояние которое нужно преодолеть') as string)
+  let watch = 0
+  let wastedTime = 0
+  while (distance >= 0){
+    wastedTime += 1
+    if (watch > 4){
+      watch = 0
+    }
+    else{
+      watch += 1
+      distance -= car.speed
+    }
+  }
+  // @ts-ignore
+  answer1.innerText = "ответ : " + wastedTime
+  console.log (wastedTime)
 }
 
 
