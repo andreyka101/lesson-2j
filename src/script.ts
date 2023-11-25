40a50f43722c358646e3ac13aabafb1921e197ba
andreyka101 <andrejkvasnin236@gmail.com>
2023-11-25 17:38:59 +0300

    fhfhhfhhfh

--- src/script.ts
+++ src/script.ts
@@ -1980,8 +1980,73 @@ executor51?.addEventListener('click', () => {
 
 
 
+  const animationBox = document.querySelector('.box_for_animation') as any
+const animationButton = document.querySelector('.button_for_animation') as any
+
+
+function startAnimation() {
+  // requestId = window.requestAnimationFrame(animate)
+  animate({timing:makeEaseOut(bounce),duration:1000,draw(progress:number) {
+    const width = document.documentElement.clientWidth - 120
+    animationBox.style.transform = `translateX(${progress*width}px)`
+  }})
+  animationButton.style.opacity = 0
+}
+// выбор анимации 
+function elastic(timeFraction:number) {
+  const x = 1.5
+  return Math.pow(2, 10 * (timeFraction - 1)) * Math.cos(20 * Math.PI * x / 3 * timeFraction)
+}
+
+function bounce(timeFraction:number) {
+  for (let a = 0, b = 1; 1; a += b, b /= 2) {
+    if (timeFraction >= (7 - 4 * a) / 11) {
+      return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+    }
+  }
+}
+
+function makeEaseOut(timing:any) {
+  return function(timeFraction:number) {
+    return 1 - timing(1 - timeFraction);
+  }
+}
+// выбор анимации ^^^^^^^^^
+
+animationButton.addEventListener('click', startAnimation, { once: true })
+
+type AnimationObj = {
+  timing: Function,
+  draw: Function,
+  duration: number
+}
+
+function animate({timing, draw, duration}:AnimationObj) {
+
+  let start = performance.now()
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction изменяется от 0 до 1
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // вычисление текущего состояния анимации
+    let progress = timing(timeFraction);
+
+    draw(progress); // отрисовать её
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
+
+
   gravityBall?.addEventListener('click', () => {
     if (gravityBall.checked) {
+      
+
       
     }
   })
